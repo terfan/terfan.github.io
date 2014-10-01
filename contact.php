e4fff00a31f5f573dc96f051b34d3c118c512e40
Teresa Fan <teresaefan@gmail.com>
2014-10-01 15:57:15 +0100

    google analytics, new form

--- contact.php
+++ contact.php
@@ -2,14 +2,14 @@
     $name = $_POST['name'];
     $email = $_POST['email'];
     $message = $_POST['message'];
-    $from = 'From: TeresasWebsite'; 
+    $from = 'contact@teresafan.com'; 
     $to = 'teresaefan@gmail.com'; 
-    $subject = 'Hello';
+    $subject = 'Hello from website!';
 
     $body = "From: $name\n E-Mail: $email\n Message:\n $message";
 
 	if ($_POST['submit']) {
-    	if (mail ($to, $subject, $body, $from)) { 
+    	if (mail($to, $subject, $body, $from)) { 
         	echo '<p>Thanks for your message, looking forward to getting in contact with you!</p>';
     	} else { 
         	echo '<p>Something went wrong, please go back and try again!</p>';
