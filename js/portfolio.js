2663fecb88ba38f625468b9e5cff8aee5a411a2d
Teresa Fan <teresaefan@gmail.com>
2015-01-08 21:18:30 -0500

    saving files before refreshing line endings

--- js/portfolio.js
+++ js/portfolio.js
@@ -1,21 +1,18 @@
-$(document).ready(function(){
-    $('.nav-link').hover(function(){
-        $(this).animate({opacity: 0.75, top: "-=5px"}, 200);
-    },function(){
-        $(this).animate({opacity: 1.0, top: "+=5px"}, 200);
-    });
-
-});
-
-function showContent(id) {
+function showContent(caller, id) {
 	$('.sub-content').fadeOut('slow');
+    $('.current-section').attr('class','sub-section');
+
+    $(caller).attr('class','current-section');
 
     setTimeout(function(){
 		$(id).fadeIn('slow');
 	}, 500);
 
-}
-
-function hideContent() {
-    $('.sub-content').fadeOut('slow');
+    if(id == '#3DModeling') {
+        $('#title').html(id.substring(1,3)+" "+id.substring(3) + "<span>Click the images for full view and more information.</span>");
+    }
+    else {
+        $('#title').html(id.substring(1) + "<span>Click the images for full view and more information.</span>");
+    }
+    
 }
