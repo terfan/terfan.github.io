6c1d3ac84f03d7d4e1bba597ea1b9cf4aa0a7b99
Teresa Fan <teresaefan@gmail.com>
2015-01-16 11:38:21 -0500

    GOT DIFFERENT TABS WORKING WITH ANIMATIONS! removed extraneous favicon file

--- js/portfolio.js
+++ js/portfolio.js
@@ -1,18 +1,47 @@
 function showContent(caller, id) {
-	$('.sub-content').fadeOut('slow');
+    $('figure').fadeOut('slow');
+    $('.credit').fadeOut('slow');
     $('.current-section').attr('class','sub-section');
 
     $(caller).attr('class','current-section');
 
-    setTimeout(function(){
-		$(id).fadeIn('slow');
-	}, 500);
-
-    if(id == '#3DModeling') {
+    if(id =='#Traditional') {
+        $('#title').html(id.substring(1) + "<span>Click the images for full view and more information.</span>");
+        setTimeout(function(){
+            //$(id).fadeIn('slow');
+            $('figure.trad').fadeIn('slow').css("display","inline-block");
+            $('.credit').fadeIn('slow').css("display","inline");
+            // temp
+            $('.photo').css("display","none");
+            $('.des').css("display","none");
+        }, 500);
+    }
+    else if(id == '#3DModeling') {
         $('#title').html(id.substring(1,3)+" "+id.substring(3) + "<span>Click the images for full view and more information.</span>");
+        setTimeout(function(){
+            //$(id).fadeIn('slow');
+            $('figure.mod').fadeIn('slow').css("display","inline-block");
+            $('.credit').fadeIn('slow').css("display","inline");
+            // temp
+            $('.photo').css("display","none");
+            $('.des').css("display","none");
+        }, 500);
+    }
+    else if(id == '#Photography') {
+        $('#title').html(id.substring(1) + "<span>Click the images for full view and more information.</span>");
+        setTimeout(function(){
+            $('.photo').fadeIn('slow').css("display","inline-block");
+            // temp
+            $('.des').css("display","none");
+        }, 500);
     }
     else {
         $('#title').html(id.substring(1) + "<span>Click the images for full view and more information.</span>");
+        setTimeout(function(){
+            $('.des').fadeIn('slow').css("display","inline-block");
+            // temp
+            $('.photo').css("display","none");
+        }, 500);
     }
     
 }
